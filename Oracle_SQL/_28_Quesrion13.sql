f39ab70f32ceb4964bc6d115518fdf4d716b3b28
jidefine <youjio2000@gmail.com>
2024-04-11 15:23:18 +0900

    28 Question13 오답 확인 중

--- Oracle_SQL/_28_Quesrion13.sql
+++ Oracle_SQL/_28_Quesrion13.sql
@@ -10,15 +10,13 @@ GROUP BY syear, major;
 
 
 2) 화학과 학년별 평균 평점을 검색하세요
-SELECT major, syear, 
-        ROUND(AVG(avr)) "평균"
+SELECT major, syear, AVG(avr) "평균"
 FROM student
 WHERE major='화학'
 GROUP BY major, syear;
 
-3) 각 학생별 기말고사 평균을 검색하세요
-SELECT st.sno, sname, 
-        ROUND(AVG(result)) "평균"
+3) 각 학생별 기말고사 평균을 검색하세요(X)
+SELECT st.sno, sname, AVG(result) "기말고사 평균"
 FROM student st
 JOIN score USING(sno)
 GROUP BY sno, result;
@@ -29,7 +27,7 @@ FROM student
 GROUP BY major;
 
 5) 화학과와 생물학과 학생 4.5 환산 평점의 평균을 각각 검색하세요
-SELECT major, ROUND(AVG(avr+0.5)) "환산평점평균"
+SELECT major, ROUND(AVG(avr+0.5), 2) "환산평점평균"
 FROM student
 WHERE major IN ('화학', '생물')
 GROUP BY major;
@@ -41,13 +39,13 @@ FROM professor
 WHERE TRUNC((sysdate-hiredate)/365) >= 10
 GROUP BY orders;
 
-7) 과목명에 화학이 포함된 과목의 학점수 총합을 검색하세요
+7) 과목명에 화학이 포함된 과목의 학점수 총합을 검색하세요(X)
 SELECT cname, SUM(st_num)
 FROM course
 WHERE cname LIKE'%화학%'
 GROUP BY cname;
 
-8) 화학과 학생들의 기말고사 성적을 성적순으로 검색하세요
+8) 화학과 학생들의 기말고사 성적을 성적순으로 검색하세요(X)
 SELECT major, st.sno, sname, sc.result
 FROM student st
 JOIN score sc USING(sno)
@@ -56,7 +54,7 @@ WHERE major='화학'
 GROUP BY major
 ORDER BY result;
 
-9) 학과별 기말고사 평균을 성적순으로 검색하세요
+9) 학과별 기말고사 평균을 성적순으로 검색하세요(X)
 SELECT major, AVG(sc.result)
 FROM student st
 JOIN score sc USING(sno)
