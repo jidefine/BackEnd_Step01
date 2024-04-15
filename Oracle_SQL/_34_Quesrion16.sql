ffb633432080a6a5e3e9764184183216c6893569
jidefine <youjio2000@gmail.com>
2024-04-15 19:56:25 +0900

    Question16 풀이완료

--- Oracle_SQL/_34_Quesrion16.sql
+++ Oracle_SQL/_34_Quesrion16.sql
@@ -1,8 +1,32 @@
 
-1) 화학과 학생과 평점이 동일한 학생들을 검색하세요
+1) 화학과 학생과 평점이 동일한 학생들을 검색하세요(?)
+SELECT major, avr
+FROM student
+WHERE major='화학';
 
+SELECT sno, sname, major, avr
+FROM student
+WHERE avr IN (SELECT avr
+                  FROM student
+                  WHERE major='화학')
+AND major!='화학';
 
 2) 화학과 교수와 부임일이 같은 직원을 검색하세요
+SELECT section, hiredate
+FROM professor
+WHERE section='화학';
 
+SELECT pno, pname, section, hiredate
+FROM professor
+WHERE hiredate IN (SELECT hiredate
+                    FROM professor
+                    WHERE section='화학')
+AND section!='화학';
 
 3) 화학과 학생과 같은 학년에서 평점이 동일한 학생들을 검색하세요
+SELECT sno, sname, major, syear, avr
+FROM student
+WHERE (syear, avr) IN (SELECT syear, avr
+                  FROM student
+                  WHERE major='화학')
+AND major!='화학';
